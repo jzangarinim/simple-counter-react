f3b089e91bb7c0cd3a21457aa324d0c8ab6461f6
Jose Zangarini <jzangarinim@gmail.com>
2023-05-09 11:08:41 -0400

    added reset and pause buttons

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Counter = () => {
+const Counter = (props) => {
+  useEffect(() => {
+    if (props.pause == 0) {
+      let interval = setInterval(() => {
+        props.setCount(props.count + props.step);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    } else if (props.pause != 0 && props.resume) {
+      let interval = setInterval(() => {
+        props.setCount(props.pause + props.count + props.step);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [props.count, props.resume]);
+
   return (
-    <div>
-      <i className="fa-regular fa-clock fa-beat fa-2xl"></i>
-    </div>
+    <>
+      <div className="d-flex justify-content-center">
+        <i className="fa-regular fa-clock fa-2xl me-3 mt-3 lh-1"></i>
+        <p className="fs-1">{props.count}</p>
+      </div>
+    </>
   );
 };
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Counter from "./Counter.jsx";
 
 //create your first component
 const Home = () => {
+  const [pause, setPause] = useState(0);
+  const [count, setCount] = useState(0);
+  const [resume, setResume] = useState(false);
+  const [step, setStep] = useState(1);
+  function handleCount(step) {
+    setCount(0);
+    setPause(0);
+    setStep(step);
+  }
+  function handlePause(counter) {
+    setPause(counter);
+    setResume(false);
+  }
+  function handleResume() {
+    setCount(0);
+    setResume(true);
+  }
+  function handleReset() {
+    setCount(0);
+    setPause(0);
+  }
+  useEffect(() => {
+    <Counter step={step} />;
+  }, [step]);
   document.title = "Counting...";
   return (
     <div className="text-center">
       <h1 className="text-center mt-5">Counter</h1>
-      <a href="#" className="btn btn-success">
-        If you see this green button... bootstrap is working...
-      </a>
-      <Counter />
+      <button onClick={() => handleCount(1)}>Step by 1</button>
+      <button onClick={() => handleCount(2)}>Step by 2</button>
+      <button onClick={() => handleCount(5)}>Step by 5</button>
+      <Counter
+        step={step}
+        setCount={setCount}
+        count={count}
+        pause={pause}
+        resume={resume}
+        setResume={setResume}
+      />
+      <button onClick={() => handleReset()}>Reset</button>
+      <button onClick={() => handlePause(count)}>Pause</button>
+      <button onClick={() => handleResume()}>Resume</button>
     </div>
   );
 };
