fcf840718acf66f8f80423afce0de3495e2efbbe
Jose Zangarini <jzangarinim@gmail.com>
2023-05-08 20:15:26 -0400

    added fontawesome 6.0 support

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <i className="fa-regular fa-clock fa-beat fa-2xl"></i>
+    </div>
+  );
+};
+
+export default Counter;
