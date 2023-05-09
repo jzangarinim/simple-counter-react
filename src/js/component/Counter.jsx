f98b39d24a3e18eece96b3101c60c22cbad14eaf
Jose Zangarini <jzangarinim@gmail.com>
2023-05-09 14:34:37 -0400

    added custom step, notification input, modified design

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect } from "react";
 
 const Counter = (props) => {
+  let num1 = Math.floor(props.count / 10000);
+  let num2 = Math.floor(props.count / 1000);
+  let num3 = Math.floor(props.count / 100);
+  let num4 = Math.floor(props.count / 10);
+  let num5 = Math.floor(props.count / 1);
   useEffect(() => {
-    if (props.pause == 0) {
+    if (props.pause === 0) {
       let interval = setInterval(() => {
         props.setCount(props.count + props.step);
       }, 1000);
       return () => {
+        if (props.notify != 0 && props.count + 1 === props.notify) {
+          alert(`Number ${props.notify} reached!`);
+        }
         clearInterval(interval);
       };
-    } else if (props.pause != 0 && props.resume) {
+    } else if (props.pause !== 0 && props.resume) {
       let interval = setInterval(() => {
-        props.setCount(props.pause + props.count + props.step);
+        props.setCount(props.pause + props.step);
+        props.setPause(props.pause + props.step);
       }, 1000);
       return () => {
         clearInterval(interval);
       };
     }
-  }, [props.count, props.resume]);
+  }, [props.count, props.pause, props.resume, props.notify]);
 
   return (
     <>
-      <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center text-white">
         <i className="fa-regular fa-clock fa-2xl me-3 mt-3 lh-1"></i>
-        <p className="fs-1">{props.count}</p>
+        <p className="fs-1">
+          {num1 % 10} {num2 % 10} {num3 % 10} {num4 % 10} {num5 % 10}
+        </p>
       </div>
     </>
   );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,17 +7,23 @@ const Home = () => {
   const [count, setCount] = useState(0);
   const [resume, setResume] = useState(false);
   const [step, setStep] = useState(1);
+  const [notify, setNotify] = useState(0);
+
   function handleCount(step) {
-    setCount(0);
-    setPause(0);
-    setStep(step);
+    if (event.target.value == 0) {
+      handleReset();
+      setStep(1);
+      event.target.value = null;
+    } else {
+      handleReset();
+      setStep(step);
+    }
   }
   function handlePause(counter) {
     setPause(counter);
     setResume(false);
   }
   function handleResume() {
-    setCount(0);
     setResume(true);
   }
   function handleReset() {
@@ -27,24 +33,65 @@ const Home = () => {
   useEffect(() => {
     <Counter step={step} />;
   }, [step]);
-  document.title = "Counting...";
+
+  document.title = `Counting... ${count}`;
+
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Counter</h1>
-      <button onClick={() => handleCount(1)}>Step by 1</button>
-      <button onClick={() => handleCount(2)}>Step by 2</button>
-      <button onClick={() => handleCount(5)}>Step by 5</button>
+    <div className="text-center bg-dark">
+      <div className="notify text-white">
+        Notify me when the counter reaches:
+        <input
+          type="number"
+          className="col-1 ms-1 mt-5 mb-3 justify-content-start"
+          onBlur={() => {
+            setNotify(Number(event.target.value));
+          }}
+        />
+      </div>
+      <h1 className="text-center text-white">Counter</h1>
+      {/* Change step */}
+      <div className="step-changer">
+        <button className="me-1" onClick={() => handleCount(1)}>
+          Step by 1
+        </button>
+        <button className="me-1" onClick={() => handleCount(2)}>
+          Step by 2
+        </button>
+        <button className="me-1" onClick={() => handleCount(5)}>
+          Step by 5
+        </button>
+        <button className="me-1" onClick={() => handleCount(10)}>
+          Step by 10
+        </button>
+        <input
+          type="number"
+          className="col-1"
+          placeholder="Custom step:"
+          onBlur={() => handleCount(Number(event.target.value))}
+        />
+      </div>
+
       <Counter
         step={step}
         setCount={setCount}
         count={count}
         pause={pause}
+        setPause={setPause}
         resume={resume}
-        setResume={setResume}
+        notify={notify}
       />
-      <button onClick={() => handleReset()}>Reset</button>
-      <button onClick={() => handlePause(count)}>Pause</button>
-      <button onClick={() => handleResume()}>Resume</button>
+      {/* Buttons */}
+      <div className="buttons pb-5">
+        <button className="me-1" onClick={() => handleReset()}>
+          Reset
+        </button>
+        <button className="me-1" onClick={() => handlePause(count)}>
+          Pause
+        </button>
+        <button className="me-1" onClick={() => handleResume()}>
+          Resume
+        </button>
+      </div>
     </div>
   );
 };
